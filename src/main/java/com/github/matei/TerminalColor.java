bd840b09774979ce3aebf47558d6069568a1380b
Matei Alexandru Gatin <gatinmatei@yahoo.com>
2026-03-09 00:00:20 +0100

    Add TerminalColor enum with 16 ANSI colors

---
+++ src/main/java/com/github/matei/TerminalColor.java
@@ -0,0 +1,29 @@
+package com.github.matei;
+
+/**
+ * Standard terminal colors.
+ * <p>
+ * Includes the 16 ANSI colors (8 normal + 8 bright) and a
+ * {@link #DEFAULT} value representing the terminal's default color.
+ */
+public enum TerminalColor {
+    DEFAULT,
+
+    BLACK,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    WHITE,
+
+    BRIGHT_BLACK,
+    BRIGHT_RED,
+    BRIGHT_GREEN,
+    BRIGHT_YELLOW,
+    BRIGHT_BLUE,
+    BRIGHT_MAGENTA,
+    BRIGHT_CYAN,
+    BRIGHT_WHITE;
+}
